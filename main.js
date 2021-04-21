f6237ea369077d82a1b45055495e489b70269b85
JisunYou <jisun.you@gmail.com>
2021-04-21 14:14:43 -0500

    master

---
+++ main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('input[name="radioQ1"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q1box").hide();
+        $("#"+inputValue).show();
+    });
+});
+
+
+$(document).ready(function(){
+    $('input[name="radioQ2"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q2box").hide();
+        $("#"+inputValue).show();
+    });
+});
+
+$(document).ready(function(){
+    $('input[name="radioQ3"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q3box").hide();
+        $("#"+inputValue).show();
+    });
+});
+$(document).ready(function(){
+    $('input[name="radioQ4"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q4box").hide();
+        $("#"+inputValue).show();
+    });
+});
+$(document).ready(function(){
+    $('input[name="radioQ5"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q5box").hide();
+        $("#"+inputValue).show();
+    });
+});
+$(document).ready(function(){
+    $('input[name="radioQ6"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q6box").hide();
+        $("#"+inputValue).show();
+    });
+});
+$(document).ready(function(){
+    $('input[name="radioQ7"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q7box").hide();
+        $("#"+inputValue).show();
+    });
+});
+$(document).ready(function(){
+    $('input[name="radioQ8"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $(".Q8box").hide();
+        $("#"+inputValue).show();
+    });
+});
